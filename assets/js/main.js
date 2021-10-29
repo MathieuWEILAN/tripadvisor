627b64ad3b32eae5ce7a13d9ddfc23b5abf14be3
MathieuWEILAN <mathieu_weilan@hotmail.fr>
2021-10-29 18:08:41 +0200

    frontend ok

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("page chargée");
+
+  //   Ouvrir le modal
+  document.getElementById("display-modal").addEventListener("click", () => {
+    document.getElementById("modal").classList.add("display-modal");
+  });
+
+  //   Fermer le modal
+  document.getElementById("remove-modal").addEventListener("click", () => {
+    document.getElementById("modal").classList.remove("display-modal");
+  });
+
+  // Envoyer le formulaire
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const data = {
+        firstname: document.getElementById("firstname").value,
+        lastname: document.getElementById("lastname").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+      };
+      console.log("ok");
+      const response = await axios.post("http://localhost:3000/", data);
+    });
+});
